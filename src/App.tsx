09c20990ffa77010bfc98802ee83ef766761752a
Brian <pacebrian0@gmail.com>
2023-11-19 13:21:26 +0100

    bugfixes

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,10 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import TodoCard from './components/TodoCard'
-import {Itodo} from './types/todo'
+import { Itodo } from './types/todo'
 
 function App() {
-  const [todos, setTodos] = useState<todo>([])
+  const [todos, setTodos] = useState<Itodo[]>([])
 
   const fetchTodos = async () => {
     const res = await fetch('https://jsonplaceholder.typicode.com/todos?_page=1')
@@ -22,7 +22,7 @@ function App() {
   return (
     <>
      <div>
-      {todos.map((x) => <TodoCard key={todo.id} todo={todo} />)}
+      {todos.map((x) => <TodoCard key={x.id} todo={x} />)}
       </div>
     </>
   )
